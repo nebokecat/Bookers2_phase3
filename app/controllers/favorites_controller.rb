34eb62e1657582e03d826e6b5c4de585977e3c6c
nebokeneko <neboke.cat@gmail.com>
2020-09-10 14:11:34 +0000

    [finish] bookers2_phase2_debug

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,22 +1,25 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_variables
   # POST /books/:book_id/favorites
   # POST /favorites.json
   def create
-    if !Favorite.find(book_id: params[:book_id], user_id: current_user.id).exists?
+    if !Favorite.find_by(book_id: params[:book_id], user_id: current_user.id)
       @favorite = current_user.favorites.new(:book_id => params[:book_id])
       @favorite.save
-      redirect_to books_path
     end
   end
 
 
   # DELETE /books/:book_id/favorites/
   def destroy
-    if Favorite.find(book_id: params[:book_id], user_id: current_user.id).exists?
+    if Favorite.find_by(book_id: params[:book_id], user_id: current_user.id)
       @favorite = Favorite.find_by(user_id: current_user, book_id: params[:book_id])
       @favorite.destroy
-      redirect_to books_path
     end
+  end
+  
+  def set_variables
+    @book = Book.find(params[:book_id])
   end
 end
